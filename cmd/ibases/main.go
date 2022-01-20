6d07ab19b6087784483627a4b3d0a2a8d8616b91
Denis Terentev <goboden@yandex.ru>
2022-01-20 14:59:44 +0300

    Load v8i file from user profile.

--- cmd/ibases/main.go
+++ cmd/ibases/main.go
@@ -1,12 +1,9 @@
 package main
 
 import (
-	"fmt"
-
 	"github.com/goboden/1c-utils-go/pkg/ibases"
 )
 
 func main() {
-	fmt.Println("Started.")
 	ibases.Run()
 }
--- pkg/ibases/app.go
+++ pkg/ibases/app.go
@@ -1,26 +1,35 @@
 package ibases
 
 import (
+	"errors"
+	"fmt"
 	"os"
 	"strings"
 )
 
 func Run() {
-	fileName := "./../../examples/ibases.v8i"
-	fileData, err := readFile(fileName)
-	if err != nil {
-		println(err.Error(), err)
-		return
+	filePath, err := findIBasesFile()
+	if err == nil {
+		fileData, err := readFile(filePath)
+		if err != nil {
+			println(err.Error(), err)
+			return
+		}
+		readIBases(fileData)
+	} else {
+		fmt.Printf("IBases file is not found: %s", err.Error())
 	}
-	readIBases(fileData)
 }
 
-func splitStrings(d []byte) []string {
-	s := strings.Split(strings.Trim(string(d), "\r\n"), "\n")
-	for i, l := range s {
-		s[i] = strings.Trim(l, "\r")
+func findIBasesFile() (string, error) {
+	var filePath string
+
+	userProfile, found := os.LookupEnv("USERPROFILE")
+	if found {
+		filePath = filePath + userProfile + "/AppData/Roaming/1C/1CEStart/ibases.v8i"
+		return filePath, nil
 	}
-	return s
+	return filePath, errors.New("Enviroment variable [USERPROFILE] is not found.")
 }
 
 func readFile(name string) (str []string, err error) {
@@ -30,4 +39,12 @@ func readFile(name string) (str []string, err error) {
 	}
 	str = splitStrings(data)
 	return str, nil
+}
+
+func splitStrings(d []byte) []string {
+	s := strings.Split(strings.Trim(string(d), "\r\n"), "\n")
+	for i, l := range s {
+		s[i] = strings.Trim(l, "\r")
+	}
+	return s
 }
